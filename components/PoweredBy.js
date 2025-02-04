03b4b2b6d885a681124dc12ee29805d90d6f7e4a
Alisa-Mona <143265368+Alisa-Mona@users.noreply.github.com>
2025-02-04 21:46:18 +0800

    更新 PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,6 +5,12 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 export default function PoweredBy(props) {
+  // 控制是否显示版权信息的条件
+  const showPoweredBy = false; // 设置为 false 时，版权信息将不会显示
+  
+  // 如果不显示版权信息，直接返回 null，不渲染任何内容
+  if (!showPoweredBy) return null;
+
   return (
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
